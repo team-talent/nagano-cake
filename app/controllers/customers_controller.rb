1e11e98b4f46591cb3b951bcd32abc9db51828f7
shunsuke <shunsuke.nakayama36@gmail.com>
2020-05-19 16:08:08 +0900

    [add]customer,signview

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,11 @@
 class CustomersController < ApplicationController
+	def hide
+		
+	end
+	def hide_update
+		@customer = current_customer
+		@customer.update(customer_status: true)
+		reset_session
+		redirect_to root_path
+	end
 end
