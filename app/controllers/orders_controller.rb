5314fd27fed357a1caeb42bdb5a92d471c4c4f79
Sato Katsushi <piano.hatsune.lesson@gmail.com>
2020-05-20 02:18:33 +0000

    コンフリクト修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,15 +3,5 @@ class OrdersController < ApplicationController
 	def index
 	end
 
-  def show
 
-  end
-
-  def new
-    @order = Order.new
-  end
-
-  def create
-    @order = Order.new
-  end
 end
