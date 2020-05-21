d0ea67f215a76afff4d637195e667d0a9fc909b2
marinamiyazawa <mi.marina1030@gmail.com>
2020-05-21 15:36:07 +0900

    マイページその他

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,19 @@
 class CustomersController < ApplicationController
+	
+	def show
+		@customer = current_customer
+	end
+
+	def edit_update
+		@customer =current_customer
+	end
+
+	def customers_update
+		@customer = current_customer
+		@customer.update(customer_params)
+		redirect_to customers_path
+	end
+
 	def hide
 		
 	end
@@ -8,4 +23,10 @@ class CustomersController < ApplicationController
 		reset_session
 		redirect_to root_path
 	end
+
+	private
+	def customer_params
+		params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :tel, :email, :postal_code, :address)
+	end
+	
 end
