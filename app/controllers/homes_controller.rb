a6f871789c5e18db9e602672096700998d74a6fb
Sato Katsushi <piano.hatsune.lesson@gmail.com>
2020-05-23 10:47:00 +0000

    change[migration]

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,6 +5,7 @@ class HomesController < ApplicationController
 	end
 
 	def thanks
+
 	end
 
 	def about
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  require 'byebug'
   def index
   end
 
@@ -12,32 +13,55 @@ class OrdersController < ApplicationController
   end
 
   def create
-
+    order = current_customer.orders.new(session[:order])
+    order.send_fee = 800
+    if order.save
+      destination = current_customer.destinations.new(
+        postcode_tosend: session[:order]["postcode_tosend"],
+        address_tosend:  session[:order]["address_tosend"],
+        name_tosend:     session[:order]["name_tosend"]
+        )
+      destination.save
+      
+      detail = Detail.new
+      detail.order_id = order.id
+      @cart = Cart.where(customer_id: current_customer)
+      @cart.each do |cart|
+        detail.product_vol = cart.vol
+        detail.product_price = cart.product.price
+      end
+      detail.save
+    else
+    end
+    redirect_to homes_thanks_path
   end
 
   def confirm
     @cart = Cart.where(customer_id: current_customer)
-    array = []
-      @cart.each do |cart|
-      array << cart.product.price * cart.vol
-    end
-    @total = array.sum
-
+    session[:order] = Order.new()
     if params[:address].to_i == 2
-      session[:order] = Order.new()
       session[:order][:pay] = params[:pay]
       session[:order][:postcode_tosend] = current_customer.postal_code
       session[:order][:address_tosend]  = current_customer.address
       session[:order][:name_tosend]     = current_customer.first_name + current_customer.last_name
     elsif params[:address].to_i == 3
 
-    else
+    elsif params[:address].to_i == 4
       session[:order] = order_params
+      session[:order][:pay] = params[:pay]
+    else
+      redirect_to orders_path
     end
+    total = 0
+      @cart.each do |cart|
+      total += cart.product.price * cart.vol
+    end
+    session[:order][:total_price] = total
+    @order = Order.new
   end
 
   private
   def order_params
-    params.require(:order).permit(:pay, :name_tosend, :postcode_tosend, :address_tosend)
+    params.require(:order).permit(:name_tosend, :postcode_tosend, :address_tosend)
   end
 end
