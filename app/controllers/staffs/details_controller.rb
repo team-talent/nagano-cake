8516e74e129b24eb39267292b19329095b1de706
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-05-24 20:45:06 +0900

    [complete]staffs_orders

--- app/controllers/staffs/details_controller.rb
+++ app/controllers/staffs/details_controller.rb
@@ -1,2 +1,12 @@
 class Staffs::DetailsController < ApplicationController
+	def update_for_productionstatus
+		@detail = Detail.find(params[:id])
+		@detail.update(detail_params)
+		redirect_to staffs_orders_path
+	end
+
+	private
+	def detail_params
+	params.require(:detail).permit(:production_status)
+	end
 end
