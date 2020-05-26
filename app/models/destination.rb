a05c85f44015a17da5134dff5d9d7ba05bd3dbc3
shunsuke <shunsuke.nakayama36@gmail.com>
2020-05-26 18:04:21 +0900

    [add]destination_auto

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -3,4 +3,14 @@ class Destination < ApplicationRecord
   validates :name_tosend, presence: true
   validates :address_tosend, presence: true
 
+    include JpPrefecture
+  jp_prefecture :prefecture_code
+
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
 end
