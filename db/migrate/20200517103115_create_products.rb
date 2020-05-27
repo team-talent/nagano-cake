fa9cb87ab5a8fde3c13ff7c58f0fb5b05c7068f3
marinamiyazawa <mi.marina1030@gmail.com>
2020-05-27 16:43:15 +0900

    fix

--- db/migrate/20200517103115_create_products.rb
+++ db/migrate/20200517103115_create_products.rb
@@ -8,6 +8,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.string :image_id
       t.text :explain
       t.integer :sale_status
+      t.integer :recommend_status
       
       t.timestamps
     end
